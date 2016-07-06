20b99d4a5c16f0e25de6679cdf75831a3f1e22a0
Yaroslav Gandzjur <gandzjur@gmail.com>
2016-07-06 10:40:41 +0300

    Add check entity and JodaTime dependency

--- src/main/java/org/gandzjur/ekey/domain/Check.java
+++ src/main/java/org/gandzjur/ekey/domain/Check.java
@@ -1,16 +1,45 @@
 package org.gandzjur.ekey.domain;
 
 import java.io.Serializable;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import org.joda.time.DateTime;
 
 @Table(name = "CHECKS")
 @Entity
 public class Check implements Serializable {
 
     @Id
+    @Column(name = "check_id", nullable = false)
     private Integer checkId;
+    
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "check_time", nullable = false, updatable = false, columnDefinition="TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private DateTime checkTime;
+
+    @ManyToOne
+    @JoinColumn(name = "room_id")
+    private Room room;
+    
+    @ManyToOne
+    @JoinColumn(name = "student_id")
+    private Student student;
+
+    public Check() {
+    }
+
+    public Check(Integer checkId, DateTime checkTime, Room room, Student student) {
+        this.checkId = checkId;
+        this.checkTime = checkTime;
+        this.room = room;
+        this.student = student;
+    }
 
     public Integer getCheckId() {
         return checkId;
@@ -18,6 +47,35 @@ public class Check implements Serializable {
 
     public void setCheckId(Integer checkId) {
         this.checkId = checkId;
+    }
+
+    public DateTime getCheckTime() {
+        return checkTime;
+    }
+
+    public void setCheckTime(DateTime checkTime) {
+        this.checkTime = checkTime;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    @Override
+    public String toString() {
+        return "Check{" + "checkId=" + checkId + ", checkTime=" + checkTime + ", room=" + room.getRoomId() + ", student=" + student.getStudentID() + '}';
     }
     
 }
