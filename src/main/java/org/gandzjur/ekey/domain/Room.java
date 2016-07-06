eef2d33c93e623ac25474334ac77716bf658bedb
Yaroslav Gandzjur <gandzjur@gmail.com>
2016-07-06 14:40:06 +0300

    Add room entity

--- src/main/java/org/gandzjur/ekey/domain/Room.java
+++ src/main/java/org/gandzjur/ekey/domain/Room.java
@@ -1,6 +1,7 @@
 package org.gandzjur.ekey.domain;
 
 import java.io.Serializable;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
@@ -14,9 +15,25 @@ public class Room implements Serializable {
     @Id
     private Integer roomId;
     
+    @Column(name = "device_mac")
+    private Long deviceMac;
+    
+    @Column(name = "roomName")
+    private String roomName;
+    
     @ManyToOne
     @JoinColumn(name = "univer_id")
     private Univercity university;
+
+    public Room() {
+    }
+
+    public Room(Integer roomId, Long deviceMac, String roomName, Univercity university) {
+        this.roomId = roomId;
+        this.deviceMac = deviceMac;
+        this.roomName = roomName;
+        this.university = university;
+    }
 
     public Integer getRoomId() {
         return roomId;
@@ -26,6 +43,22 @@ public class Room implements Serializable {
         this.roomId = roomId;
     }
 
+    public Long getDeviceMac() {
+        return deviceMac;
+    }
+
+    public void setDeviceMac(Long deviceMac) {
+        this.deviceMac = deviceMac;
+    }
+
+    public String getRoomName() {
+        return roomName;
+    }
+
+    public void setRoomName(String roomName) {
+        this.roomName = roomName;
+    }
+
     public Univercity getUniversity() {
         return university;
     }
@@ -33,5 +66,10 @@ public class Room implements Serializable {
     public void setUniversity(Univercity university) {
         this.university = university;
     }
-    
+
+    @Override
+    public String toString() {
+        return "Room{" + "roomId=" + roomId + ", deviceMac=" + deviceMac + ", roomName=" + roomName + ", university=" + university.getUniverName() + '}';
+    }
+
 }
