a02a1cf88f827dc6724c8bc8909abc6763295397
Yaroslav Gandzjur <gandzjur@gmail.com>
2016-07-06 10:21:41 +0300

    Add group entity

--- src/main/java/org/gandzjur/ekey/domain/Group.java
+++ src/main/java/org/gandzjur/ekey/domain/Group.java
@@ -1,8 +1,11 @@
 package org.gandzjur.ekey.domain;
 
 import java.io.Serializable;
+import java.util.Set;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Table(name = "GROUPS")
@@ -10,7 +13,23 @@ import javax.persistence.Table;
 public class Group implements Serializable {
 
     @Id
+    @Column(name = "group_id", nullable = false)
     private Integer groupId;
+    
+    @Column(name = "group_name", nullable = false)
+    private String groupName;
+    
+    @OneToMany(mappedBy = "group")
+    private Set<Student> students;
+
+    public Group() {
+    }
+
+    public Group(Integer groupId, String groupName, Set<Student> students) {
+        this.groupId = groupId;
+        this.groupName = groupName;
+        this.students = students;
+    }
 
     public Integer getGroupId() {
         return groupId;
@@ -19,5 +38,26 @@ public class Group implements Serializable {
     public void setGroupId(Integer groupId) {
         this.groupId = groupId;
     }
-    
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+    public Set<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(Set<Student> students) {
+        this.students = students;
+    }
+
+    @Override
+    public String toString() {
+        return "Group{" + "groupId=" + groupId + ", groupName=" + groupName + ", students=" + students.toString() + '}';
+    }
+
 }
