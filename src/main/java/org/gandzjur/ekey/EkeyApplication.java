80d5cfa21eee3d5d3421c0953599c305d88e6b36
Yaroslav Gandzjur <gandzjur@gmail.com>
2016-07-11 10:17:30 +0300

    Clean

--- src/main/java/org/gandzjur/ekey/EkeyApplication.java
+++ src/main/java/org/gandzjur/ekey/EkeyApplication.java
@@ -1,14 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.gandzjur.ekey;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan
